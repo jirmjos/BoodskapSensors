d19ac9c5d26480dfcba6656c047df5cdd3ab8854
Jegan Vincent <jvincent@boodskap.io>
2017-03-27 15:19:34 +0530

    IoTfied the voltage sensor

--- lib/BoodskapThreePhaseVoltageSensor/examples/VoltageSensor/VoltageSensor.ino
+++ lib/BoodskapThreePhaseVoltageSensor/examples/VoltageSensor/VoltageSensor.ino
@@ -1,41 +1,89 @@
-#include <Arduino.h>
-#include "ThreePhaseVoltageSensor.h"
+#include <BoodskapConfig.h>
+#include <BoodskapMessage.h>
+#include <BoodskapNodeMCUCommunicator.h>
+#include <ThreePhaseVoltageSensor.h>
 
 #define PIN_1 D5
 #define PIN_2 D6
 #define PIN_3 D7
 
+void commandReceived(BoodskapMessage* cmd);
 void voltageChanged(bool p1, bool p2, bool p3);
 
+BoodskapNodeMCUCommunicator boodskap(&commandReceived);
 ThreePhaseVoltageSensor vMeter(PIN_1, PIN_2, PIN_3, &voltageChanged);
 
-uint32_t elapsed = 0;
+uint32_t lastMessage = 0;
 
 void setup() {
-  Serial.begin(115200);
-  delay(1000);
+
+  Serial.begin(BAUD_RATE);
+
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  delay(3500);
+
+  DEBUG_PRINTLN("Boodskap 3Phase VSensor Starting....");
+  DEBUG_PRINTLN("");
+
+  boodskap.setup();
   vMeter.setup();
-  Serial.println();
-  Serial.println();
-  Serial.println();
+
+  DEBUG_PRINTLN("");
+  DEBUG_PRINTLN(BOODSKAP_FIRMWARE_VERSION);
+  DEBUG_PRINTLN("Setup complete.");
+
 }
 
 void loop() {
 
+  boodskap.loop();
   vMeter.loop();
 
-  if ((millis() - elapsed) >= 15000) {
-    elapsed = millis();
-    Serial.println("Heartbeat");
+  if (boodskap.isConnected() && (millis() - lastMessage >= 25000)) {
+
+    BoodskapMessage message;
+    bool p1;
+    bool p2;
+    bool p3;
+
+    vMeter.voltage(&p1, &p2, &p3);
+
+    boodskap.create(999, &message);
+
+    message.set("alive", (uint32_t)millis());
+    message.set("p1", p1);
+    message.set("p2", p2);
+    message.set("p3", p3);
+
+    boodskap.send(&message);
+
+    lastMessage = millis();
   }
+
+}
+
+void commandReceived(BoodskapMessage* cmd)
+{
+  DEBUG_PRINTLN("Command received...");
+  cmd->get().printTo(Serial);
+  DEBUG_PRINTLN("");
 }
 
 void voltageChanged(bool p1, bool p2, bool p3)
 {
-  Serial.print("P1 : ");
-  Serial.print(p1);
-  Serial.print(" P2 : ");
-  Serial.print(p2);
-  Serial.print(" P3 : ");
-  Serial.println(p3);
+
+  BoodskapMessage message;
+  boodskap.create(999, &message);
+  message.set("alive", (uint32_t)millis());
+  message.set("p1", p1);
+  message.set("p2", p2);
+  message.set("p3", p3);
+
+  boodskap.send(&message);
+
+  lastMessage = millis();
+
 }
